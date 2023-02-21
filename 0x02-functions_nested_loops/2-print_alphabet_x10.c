1d6badec475c8aa00d3be1cdc700fc7382a5ec4b
sharon254 <sharonknyabuti@gmail.com>
2023-02-21 07:19:58 -0800

    char

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,8 @@
 #include "main.h"
-/*
- *print_alphabet_x10 - Make alphabet x10 times
- *
+/*print_alphabet_x10 - Make alphabet x10 times
+ * 
  *Return: void
-*/
+ */
 void print_alphabet_x10(void)
 {
 	char c;
