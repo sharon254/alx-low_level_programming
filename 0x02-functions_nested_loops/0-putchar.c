23678478fc30a427b3c94239e78a767c42955df9
sharon254 <sharonknyabuti@gmail.com>
2023-02-21 06:36:35 -0800

    trial

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,14 +5,17 @@
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
 	_putchar('t');
-	 _putchar('c');
-	  _putchar('h');
-	   _putchar('a');
-	    _putchar('r');
-	     _putchar('\n');
-	     return (0);
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}
 
