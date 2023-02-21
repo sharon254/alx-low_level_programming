5005599873d1c859a202f5e9896b9cd08fcc855c
sharon254 <sharonknyabuti@gmail.com>
2023-02-21 06:53:36 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+ *print_alphabet_x10 - Make alphabet x10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char i;
+	int j =0;
+	while(j<9)
+	{
+		for (i='a';i<='z';i++){
+		_putchar(i);
+		_putchar('\n');
+		j++;}
+	}
+}
