b9b6a943b5d29a25c2416f5c5a9777eb4dbc29c8
sharon254 <sharonknyabuti@gmail.com>
2023-02-27 18:23:38 -0800

    add

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * reset_to_98 - checks the code
- *
- * @n: is pointer to an int.
- *
- * Return: Always 0.
- */
+* reset_to_98 - checks the code
+*
+* @n: is pointer to an int.
+* Return: Always 0.
+*/
 void reset_to_98(int *n)
 {
 *n = 98;
-}  
+}
