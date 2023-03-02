b663c9e3b25b74874002797960a9f0e6def9e097
sharon254 <sharonknyabuti@gmail.com>
2023-03-02 10:29:47 -0800

    edit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
- * _strcat - a function that concatenates two strings
- * @dest: copy to
- * @src: copy from
- * Return: two concat strings
- */
+* _strcat - a function that concatenates two strings
+* @dest: copy to
+* @src: copy from
+* Return: two concat strings
+*/
 char *_strcat(char *dest, char *src)
 {
 	/* declare a pointer to store the address of dest */
