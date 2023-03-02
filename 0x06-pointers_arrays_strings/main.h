c64db44307974fab459d491ad0959e787b0df204
sharon254 <sharonknyabuti@gmail.com>
2023-03-02 10:24:26 -0800

    edit

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+char _putchar(char c);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *cap_string(char *);
+char *string_toupper(char *);
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_number(int n);
+char *rot13(char *);
+
+
+#endif
